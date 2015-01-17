e5c95a798afaa8391fb248e3e9e47583ef6b113f
Jack Dalton <jackdalton@users.noreply.github.com>
2015-01-16 20:55:24 -0700

    Comments added
    Comments added on unclear lines to further clarify the meaning of variables and functions.

--- un.js
+++ un.js
@@ -3,8 +3,8 @@
 // No third-party libraries.
 // Two assets (un.png; obama.png)
 function init() { // called on page load
-    c = document.getElementById("canvas");
-    ctx = c.getContext("2d");
+    c = document.getElementById("canvas"); // get canvas
+    ctx = c.getContext("2d"); // get context
     scoreLoop();
     gameloop();
     detectHits();
@@ -12,31 +12,31 @@ function init() { // called on page load
 var c, ctx, unX = [], unY = [],
     then = Date.now(),
     lost = false,
-    score = 0;
-var obama = {
+    score = 0; // misc vars
+var obama = { // main obama var
     x:240,
     y:460,
     render:function() {
-        ctx.drawImage(obamaImage, this.x, this.y, 25, 33);
+        ctx.drawImage(obamaImage, this.x, this.y, 25, 33); // draws image of obama
     }
 };
-var un = {
+var un = { // main un var
     render:function() {
         for (var i = 0; i < unX.length; i++) {
-            ctx.drawImage(unImage, unX[i], unY[i], 25, 29.619565217);
+            ctx.drawImage(unImage, unX[i], unY[i], 25, 29.619565217); // draws image of un
         }
     },
-    generate:function() {
+    generate:function() { // generates new instances of un
         unX.push(rand(40, 460));
         unY.push(rand(0, -10));
     },
-    drop:function() {
+    drop:function() { // makes un instances fall
         for (var i = 0; i < unY.length; i++) {
             unY[i] += 2;
         }
     }
 };
-function detectHits() {
+function detectHits() { // detects collisions between obama and un
     for (var i = 0; i < unX.length; i++) {
         if (
             obama.x <= (unX[i] + 25) &&
@@ -44,46 +44,46 @@ function detectHits() {
             obama.y <= (unY[i] + 29.619565217) &&
             unY[i] <= (obama.y + 33)
         ) {
-            lost = true;
+            lost = true; // if player touches an instance of un, player loses
         }
     }
-    window.requestAnimationFrame(detectHits);
+    window.requestAnimationFrame(detectHits); // does it all again
 }
-function scoreLoop() {
-    if (!lost) {
+function scoreLoop() { // main score loop
+    if (!lost) { // if player hasn't lost yet, their score increases
         score++;
-        setTimeout(scoreLoop, 100);
+        setTimeout(scoreLoop, 100); // does it all again every 10th of a second
     }
 }
-function gameloop() {
-    c.width = c.width;
-    if (!lost) {
-        un.render();
-        obama.render();
-        if (chance5()) {
+function gameloop() { // main game loop
+    c.width = c.width; // clears canvas
+    if (!lost) { // if player hasn't lost
+        un.render(); // renders un
+        obama.render(); // renders obama
+        if (chance5()) { // 5% chance of un instance being generated
             un.generate();
         }
-        un.drop();
-        var now = Date.now();
-        var delta = now - then;
-        move(delta / 1000);
-        then = now;
-        ctx.fillStyle = "red";
-        ctx.fillText("Score:" + score, 10, 10);
+        un.drop(); // makes the fat man fall :)
+        var now = Date.now(); // time delta stuff *
+        var delta = now - then; //*
+        move(delta / 1000); // *
+        then = now; // *
+        ctx.fillStyle = "red"; // sets text color
+        ctx.fillText("Score:" + score, 10, 10); // displays score
     }
     else {
         lose();
     }
-    window.requestAnimationFrame(gameloop);
+    window.requestAnimationFrame(gameloop); // does it all again
 }
-function lose() {
+function lose() { // displays loss message
     document.getElementById("lostmessage").style.opacity = 1;
     document.getElementById("lostmessagetext").innerHTML = "Game Over!<br>Score:" + score + ".";
 }
-function rand(min, max) {
+function rand(min, max) { //  random number generator
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
-function removeItem(array, item) {
+function removeItem(array, item) { // removes item from given array
     var e = array.indexOf(item);
     if (e > -1) {
         array.splice(e, 1);
@@ -91,13 +91,13 @@ function removeItem(array, item) {
 }
 var move = function(m) { // movement function
     if (37 in keysDown && obama.x > 10) { // if left arrow key pressed, move left
-        obama.x -= 180 * m;
+        obama.x -= 180 * m; // obama movement
     }
     if (39 in keysDown && obama.x < 485) { // if right arrow key pressed, move right
-        obama.x += 180 * m;
+        obama.x += 180 * m; // obama movement
     }
 };
-function chance50() {
+function chance50() { // 50 percent chance
     if (rand(1,2) == 1) {
         return true;
     }
@@ -105,7 +105,7 @@ function chance50() {
         return false;
     }
 }
-function chance5() {
+function chance5() { // 5 percent chance
     if (rand(1,20) == 1) {
         return true;
     }
@@ -113,7 +113,7 @@ function chance5() {
         return false;
     }
 }
-var keysDown = {};
+var keysDown = {}; // keys pressed
 addEventListener("keydown", function(e) { // checks for keys down
     keysDown[e.keyCode] = true;
 }, false);
