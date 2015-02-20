40a16402b8f2419501200c3684c18cdb8a3b45e2
Jack Dalton <jackdalton@users.noreply.github.com>
2015-02-19 20:12:22 -0700

    Create submit_score.php

---
+++ submit_score.php
@@ -0,0 +1,11 @@
+<?php
+    $score = $_POST["score"];
+    $user = $_POST["user"];
+    $scoreFile = file_get_contents("scores.txt");
+    date_default_timezone_set("America/Denver");
+    $time = date("g:i:s A e");
+    $date = date("D, M j, Y");
+    $scoreAddition = $score . "|" . $user . "|" . $time . "|" . $date;
+    file_put_contents("scores.txt", $scoreAddition);
+    echo "<script>window.location.assign('scores.txt');</script>";
+?>
